71609a6ff28c07835a1bb3b54e85b1dadb774b23
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-21 09:24:02 -0300

    validação do formulário 5 feita

--- formularioorcamento/src/views/Questionario5.vue
+++ formularioorcamento/src/views/Questionario5.vue
@@ -1,20 +1,18 @@
 <template>
   <div>
     <div class="row">
-      <div
-        class="message"
-      >Sabemos que seu tempo é escasso! Por isso, nosso questionário foi montado sob medida com base em suas seleções na tela anterior! Na Postali Seguros buscaremos as melhores ofertas disponíveis no mercado, grátis e sem compromisso. Fique tranquilo! Cuidaremos dos seus dados e não enviaremos Spam! Também não gostamos disso!</div>
+      <div class="message">
+        Sabemos que seu tempo é escasso! Por isso, nosso questionário foi montado sob medida com base em suas seleções
+        na tela anterior! Na Postali Seguros buscaremos as melhores ofertas disponíveis no mercado, grátis e sem
+        compromisso. Fique tranquilo! Cuidaremos dos seus dados e não enviaremos Spam! Também não gostamos disso!
+      </div>
     </div>
     <div class="row mb-4">
       <div class="col-6 text-right mb-3 d-flex align-items-center">
         <label for="valorSeguro">Selecione o valor do Seguro de Vida de sua preferência:</label>
       </div>
       <div class="col-6 text-left mb-3 d-flex align-items-center">
-        <select
-          name="valorSeguro"
-          id="valorSeguro"
-          v-model="cotacao.questionarios['5'].valorSeguro"
-        >
+        <select name="valorSeguro" id="valorSeguro" v-model="cotacao.questionarios['5'].valorSeguro">
           <option value="30000">R$ 30.000</option>
           <option value="40000">R$ 40.000</option>
           <option value="50000">R$ 50.000</option>
@@ -41,11 +39,7 @@
         <label for="rendaMensal">Selecione a sua faixa de renda mensal:</label>
       </div>
       <div class="col-6 text-left mb-3 d-flex align-items-center">
-        <select
-          name="rendaMensal"
-          id="rendaMensal"
-          v-model="cotacao.questionarios['5'].rendaMensal"
-        >
+        <select name="rendaMensal" id="rendaMensal" v-model="cotacao.questionarios['5'].rendaMensal">
           <option value="Até R$ 2.000">Até R$ 2.000</option>
           <option value="R$ 2.001 e R$ 3.000">R$ 2.001 e R$ 3.000</option>
           <option value="R$ 3.001 a R$ 5.000">R$ 3.001 a R$ 5.000</option>
@@ -66,24 +60,12 @@
         <div class="row w-100">
           <div class="col">
             <label for="nao">
-              <input
-                type="radio"
-                id="nao"
-                name="fumante"
-                value="nao"
-                v-model="cotacao.questionarios['5'].fumante"
-              />Não
+              <input type="radio" id="nao" name="fumante" value="nao" v-model="cotacao.questionarios['5'].fumante" />Não
             </label>
           </div>
           <div class="col">
             <label for="sim">
-              <input
-                type="radio"
-                id="sim"
-                name="fumante"
-                value="sim"
-                v-model="cotacao.questionarios['5'].fumante"
-              />Sim
+              <input type="radio" id="sim" name="fumante" value="sim" v-model="cotacao.questionarios['5'].fumante" />Sim
             </label>
           </div>
         </div>
@@ -97,21 +79,51 @@
 <script>
 // @ is an alias to /src
 export default {
-  name: "Home",
+  name: 'Home',
   data: () => ({
     cotacao: {
-      questionarios: {}
-    }
+      questionarios: {},
+    },
   }),
 
+  computed: {
+    fumanteValidation() {
+      if (!this.cotacao.questionarios['5'].fumante) {
+        return {
+          valido: false,
+          mensagemErro: 'Você precisa nos informar se é fumante ou não',
+        };
+      } else {
+        return { valido: true };
+      }
+    },
+
+    formValidation() {
+      const campos = ['fumante'];
+      for (let campo of campos) {
+        const campoValidation = this[`${campo}Validation`];
+        if (!campoValidation.valido) {
+          return {
+            valido: false,
+            mensagemErro: campoValidation.mensagemErro,
+          };
+        }
+      }
+
+      return {
+        valido: true,
+      };
+    },
+  },
+
   beforeMount() {
-    if (JSON.parse(localStorage.getItem("cotacao"))) {
-      let cotacao = JSON.parse(localStorage.getItem("cotacao"));
-      if (cotacao.questionarios["5"] == null) {
-        cotacao.questionarios["5"] = {
+    if (JSON.parse(localStorage.getItem('cotacao'))) {
+      let cotacao = JSON.parse(localStorage.getItem('cotacao'));
+      if (cotacao.questionarios['5'] == null) {
+        cotacao.questionarios['5'] = {
           valorSeguro: 30000,
-          rendaMensal: "Até R$ 2.000",
-          fumante: null
+          rendaMensal: 'Até R$ 2.000',
+          fumante: null,
         };
       }
       this.cotacao = cotacao;
@@ -119,20 +131,31 @@ export default {
       this.$router.push(`/`);
     }
   },
+
   methods: {
     voltar() {
       this.$router.back();
     },
+
     avancar() {
-      localStorage.setItem("cotacao", JSON.stringify(this.cotacao));
+      const formValidation = this.formValidation;
+      if (!formValidation.valido) {
+        return this.$swal.fire({
+          icon: 'error',
+          title: 'Ops...',
+          text: formValidation.mensagemErro,
+        });
+      }
+
+      localStorage.setItem('cotacao', JSON.stringify(this.cotacao));
       let proximo;
       for (const quest of Object.keys(this.cotacao.questionarios).reverse()) {
         if (quest > 5) {
           proximo = `/questionario/${quest}`;
         }
       }
-      this.$router.push(proximo || "/contato");
-    }
-  }
+      this.$router.push(proximo || '/contato');
+    },
+  },
 };
 </script>
