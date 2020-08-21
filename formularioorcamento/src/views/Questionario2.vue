bacc741a7d8575c661c98cb2f1f217f52cccd2c0
TiagoDiass <tiago.costadiasss@gmail.com>
2020-08-20 23:07:10 -0300

    validação questionário 2 de vdd feita

--- formularioorcamento/src/views/Questionario2.vue
+++ formularioorcamento/src/views/Questionario2.vue
@@ -8,30 +8,47 @@
       </div>
     </div>
     <div class="row mb-4">
+      <!-- CEP -->
       <div class="col-12 text-left d-flex align-items-center">
         <label for="cep">CEP</label>
       </div>
-      <div class="col-12 text-left mb-3 d-flex align-items-center">
+      <div @click="contadores.cep++" class="col-12 text-left mb-3 d-flex flex-column align-items-end">
         <the-mask
+          :class="{ 'mb-1': !cepValidation.valido && contadores.cep != 0 }"
           type="text"
           id="cep"
           placeholder="Informe seu CEP"
           :mask="['#####-###']"
           v-model="cotacao.questionarios['2'].cep"
         />
+        <small v-if="!cepValidation.valido && contadores.cep != 0" class="feedback-invalido mb-1">
+          {{ cepValidation.mensagemErro }}
+        </small>
       </div>
+
+      <!-- NÚMERO -->
       <div class="col-12 text-left d-flex align-items-center">
         <label for="numero">Número</label>
       </div>
-      <div class="col-12 text-left mb-3 d-flex align-items-center">
-        <input type="text" id="numero" placeholder="Número" v-model="cotacao.questionarios['2'].numero" />
+      <div class="col-12 text-left mb-3 d-flex flex-column align-items-end">
+        <input
+          @focus="contadores.numero++"
+          type="text"
+          id="numero"
+          placeholder="Número"
+          v-model="cotacao.questionarios['2'].numero"
+        />
+        <small v-if="!numeroValidation.valido && contadores.numero != 0" class="feedback-invalido mb-1">
+          {{ numeroValidation.mensagemErro }}
+        </small>
       </div>
 
+      <!-- PISO -->
       <div class="col-12 text-left d-flex align-items-center">
         <label for="piso">Piso:</label>
       </div>
-      <div class="col-12 text-left mb-3 d-flex align-items-center">
-        <select name="piso" id="piso" v-model="cotacao.questionarios['2'].piso">
+      <div class="col-12 text-left mb-3 d-flex flex-column align-items-end">
+        <select @focus="contadores.piso++" name="piso" id="piso" v-model="cotacao.questionarios['2'].piso">
           <option :value="null" disabled>Selecione o piso</option>
           <option value="Sobrado">Sobrado</option>
           <option value="Subsolo">Subsolo</option>
@@ -39,6 +56,9 @@
           <option value="A partir 2 andar">A partir 2 andar</option>
           <option value="Mezanino">Mezanino</option>
         </select>
+        <small v-if="!pisoValidation.valido && contadores.piso != 0" class="feedback-invalido mb-1">
+          {{ pisoValidation.mensagemErro }}
+        </small>
       </div>
     </div>
 
@@ -54,6 +74,12 @@ export default {
   data: () => ({
     cotacao: {
       questionarios: {},
+    },
+
+    contadores: {
+      cep: 0,
+      numero: 0,
+      piso: 0,
     },
   }),
 
@@ -73,7 +99,59 @@ export default {
     }
   },
 
-  computed: {},
+  computed: {
+    cepValidation() {
+      if (!this.cotacao.questionarios['2'].cep) {
+        return {
+          valido: false,
+          mensagemErro: 'CEP é obrigatório',
+        };
+      } else if (this.cotacao.questionarios['2'].cep.length < 8) {
+        return {
+          valido: false,
+          mensagemErro: 'CEP deve conter 8 digítos',
+        };
+      } else {
+        return { valido: true };
+      }
+    },
+
+    numeroValidation() {
+      if (!this.cotacao.questionarios['2'].numero || this.cotacao.questionarios['2'].numero < 0) {
+        return {
+          valido: false,
+          mensagemErro: 'Número é obrigatório e deve ser maior que 0',
+        };
+      } else {
+        return {
+          valido: true,
+        };
+      }
+    },
+
+    pisoValidation() {
+      if (!this.cotacao.questionarios['2'].piso) {
+        return { valido: false, mensagemErro: 'Piso é obrigatório' };
+      } else {
+        return { valido: true };
+      }
+    },
+
+    formValidation() {
+      const campos = ['cep', 'numero', 'piso'];
+      for (let campo of campos) {
+        const campoValidation = this[`${campo}Validation`];
+        if (!campoValidation.valido) {
+          return {
+            valido: false,
+            mensagemErro: campoValidation.mensagemErro,
+          };
+        }
+      }
+
+      return { valido: true };
+    },
+  },
 
   methods: {
     voltar() {
@@ -81,6 +159,16 @@ export default {
     },
 
     avancar() {
+      const formValidation = this.formValidation;
+
+      if (!formValidation.valido) {
+        return this.$swal.fire({
+          icon: 'error',
+          title: 'Ops...',
+          text: formValidation.mensagemErro,
+        });
+      }
+
       localStorage.setItem('cotacao', JSON.stringify(this.cotacao));
       let proximo;
       for (const quest of Object.keys(this.cotacao.questionarios).reverse()) {
